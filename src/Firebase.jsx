ecd86c7935bfbc2a310923f426ec53bd63ad2e00
Olina Kundu <olinakundu55@gmail.com>
2025-04-21 23:42:50 +0530

    Update Firebase.jsx

--- src/Firebase.jsx
+++ src/Firebase.jsx
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID,
-    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
-};
+    apiKey: "AIzaSyBASN7mcUEMcjEZ5YoOoryScQrkCwEGR5Y",
+    authDomain: "hulo-25502.firebaseapp.com",
+    projectId: "hulo-25502",
+    storageBucket: "hulo-25502.firebasestorage.app",
+    messagingSenderId: "325794264733",
+    appId: "1:325794264733:web:a84ffec1e486d3ec6cc674",
+    measurementId: "G-HEC7Q5VHTE"
+  };
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
