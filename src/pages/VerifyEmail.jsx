56a497d94b68148a5509ad335a6be00ed3c39a76
ArijitKRoy <83859073+arijitkroy@users.noreply.github.com>
2025-04-22 18:15:15 +0530

    Update VerifyEmail.jsx

--- src/pages/VerifyEmail.jsx
+++ src/pages/VerifyEmail.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import "./css/VerifyEmail.css";
 import { app } from "../Firebase";
+import { bake_cookie } from "sfcookies";
 
 const VerifyEmail = () => {
     const auth = getAuth(app);
